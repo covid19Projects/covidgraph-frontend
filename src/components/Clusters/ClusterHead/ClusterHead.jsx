43a5a83ab68b7fd125db839bdfa4c4f0f0ec98f1
Sheik Arbaz <sheik.arbaz@thoughtworks.com>
2020-03-30 16:43:11 +0530

    Add Create Cluster

--- src/components/Clusters/ClusterHead/ClusterHead.jsx
+++ src/components/Clusters/ClusterHead/ClusterHead.jsx
@@ -2,35 +2,39 @@ import React, { Component } from "react";
 import search from "../../../assets/covid-search.svg";
 import menu from "../../../assets/covid-menu.svg";
 import add from "../../../assets/covid-add.svg";
-import "./ClusterHead.scss";
+import './ClusterHead.scss';
+import CreateClusterContent from "../../DropButtonBody/CreateClusterContent.jsx";
+import { Grommet, Box, Button, DropButton, Heading, Text } from "grommet";
 
 class ClusterHead extends Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {showCreatePopUp: false};
+    }
 
-  render() {
-    return (
-      <div className="cluster-header">
-        <div className="description">
-          <p className="title">Clusters</p>
-          <p className="body">Lorem Ipsum is simply dummy text.</p>
-        </div>
-        <div className="controls">
-          <button className="icon">
-            <img className="image" src={search} />
-          </button>
-          <button className="icon">
-            <img className="image" src={menu} />
-          </button>
-          <button className="icon">
-            <img className="image" src={add} />
-          </button>
-          <button className="icon">Create Cluster</button>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="cluster-header">
+                <div className="description">
+                    <p className="title">Clusters</p>
+                </div>
+                <div className="controls">
+                    <button className="icon"><img className="image" src={search}/></button>
+                    <button className="icon"><img className="image" src={menu}/></button>
+                    <button className="icon"><img className="image" src={add}/></button>
+                    <DropButton
+                        className="icon"
+                        label="Create Cluster"
+                        // open={open}
+                        onOpen={() =>this.setState({showCreatePopUp: true})}
+                        onClose={() =>this.setState({showCreatePopUp: false})}
+                        dropContent={<CreateClusterContent onClose={() =>this.setState({showCreatePopUp: false})} />}
+                        dropProps={{ align: { top: "bottom" } }}
+                    />
+                </div>
+            </div>
+        )
+    }
 }
 
 export default ClusterHead;
---
+++ src/components/DropButtonBody/CreateClusterContent.jsx
@@ -0,0 +1,52 @@
+import React, {Component} from "react";
+import {Box, Button, Form, FormField, Select, TextInput} from "grommet";
+import "./CreateClusterContent.scss"
+
+class CreateClusterContent extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div>
+                <Box
+                    width="large"
+                    direction="row"
+                    align="center"
+                    pad={{ horizontal: "small", vertical: "xsmall" }}
+                    round="small"
+                    elevation={"medium" }
+                    border={{
+                        side: "all",
+                        color: "border"
+                    }}
+                    style={
+                        {
+                            borderBottomLeftRadius: "0px",
+                            borderBottomRightRadius: "0px"
+                        }
+                    }
+                >
+                <Form>
+                    <FormField label="Select a Cluster Title">
+                        <Select
+                            options={['AndhraPradesh', 'Telangana', 'Punjab', 'Kerala']}
+                            value={""}
+                            placeholder={"ex: State(AP)"}
+                            onChange={({ option }) => {}}
+                        />
+                    </FormField>
+                    <FormField label="Cluster Name">
+                        <TextInput plain placeholder="ex: Vijayawada" />
+                    </FormField>
+                    <Button type="submit"  label="Cancel" />
+                    <Button type="submit" primary label="Submit" />
+                </Form>
+            </Box>
+            </div>
+        )
+    }
+
+}
+export default CreateClusterContent;
