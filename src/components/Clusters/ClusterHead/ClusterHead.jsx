014637edb88492e8426630e676a6a64dfaf8a723
Sheik Arbaz <sheik.arbaz@thoughtworks.com>
2020-03-30 16:43:11 +0530

    Add Create Cluster

--- src/components/Clusters/ClusterHead/ClusterHead.jsx
+++ src/components/Clusters/ClusterHead/ClusterHead.jsx
@@ -3,10 +3,13 @@ import search from "../../../assets/covid-search.svg";
 import menu from "../../../assets/covid-menu.svg";
 import add from "../../../assets/covid-add.svg";
 import './ClusterHead.scss';
+import CreateClusterContent from "../../DropButtonBody/CreateClusterContent.jsx";
+import { Grommet, Box, Button, DropButton, Heading, Text } from "grommet";
 
 class ClusterHead extends Component {
     constructor(props) {
         super(props);
+        this.state = {showCreatePopUp: false};
     }
 
     render() {
@@ -19,12 +22,19 @@ class ClusterHead extends Component {
                     <button className="icon"><img className="image" src={search}/></button>
                     <button className="icon"><img className="image" src={menu}/></button>
                     <button className="icon"><img className="image" src={add}/></button>
-                    <button className="icon">Create Cluster</button>
+                    <DropButton
+                        className="icon"
+                        label="Create Cluster"
+                        // open={open}
+                        onOpen={() =>this.setState({showCreatePopUp: true})}
+                        onClose={() =>this.setState({showCreatePopUp: false})}
+                        dropContent={<CreateClusterContent onClose={() =>this.setState({showCreatePopUp: false})} />}
+                        dropProps={{ align: { top: "bottom" } }}
+                    />
                 </div>
             </div>
         )
     }
-
 }
 
 export default ClusterHead;
